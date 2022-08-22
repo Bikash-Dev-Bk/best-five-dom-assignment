e1af67834ade8dead61b3d702ad3665053f51f8a
bikash-dev <bikash.dev.bk@gmail.com>
2022-08-22 19:57:58 +0600

    selected five added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,74 @@
 
+function getPlayerNameById(playerNameId){
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement("li");
+
+    const playerName = document.getElementById(playerNameId);
+    const playerNameValue = playerName.innerText;
+    li.innerText = playerNameValue;
+    listContainer.appendChild(li);
+}
+
+
+// players select button click
+
+document.getElementById('btn-messi').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-messi');
+})
+
+document.getElementById('btn-neymar').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-neymar');
+})
+
+document.getElementById('btn-kimpembe').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-kimpembe');
+})
+
+document.getElementById('btn-ramos').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-ramos');
+})
+
+document.getElementById('btn-marquinhos').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-marquinhos');
+})
+
+document.getElementById('btn-verratti').addEventListener('click', function(){
+    const playerMessi = getPlayerNameById('player-verratti');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// total Amount calaculate
+
 function inputFieldValueById(inputFieldId){
     const inputValue = document.getElementById(inputFieldId);
     const inputValueString = inputValue.value;
