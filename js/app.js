56b4bb7a47506b41e762fe9d0cbcf50ee5a6e040
bikash-dev <bikash.dev.bk@gmail.com>
2022-08-22 17:08:48 +0600

    budget js added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('best five added');
+
+function inputFieldValueById(inputFieldId){
+    const inputValue = document.getElementById(inputFieldId);
+    const inputValueString = inputValue.value;
+    const inputValueAmount = parseInt(inputValueString);
+
+    return inputValueAmount;
+}
+
+
+
+document.getElementById('btn-calculate-total').addEventListener('click', function(){
+    const playerExpenses = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpenses.innerText;
+    const totalPlayerExpenses = parseInt(playerExpensesString);
+
+    const managerAmount = inputFieldValueById('manager-amount');
+    const coachAmount = inputFieldValueById('coach-amount');
+
+    const totalAmount = totalPlayerExpenses + managerAmount + coachAmount;
+
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = totalAmount;
+})
